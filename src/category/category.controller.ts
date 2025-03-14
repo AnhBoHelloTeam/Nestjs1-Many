d89dded756140e7f3904a56b3eecb77a4355eae0
AnhBoHelloTeam <nhanhateku1@gmail.com>
2025-03-14 15:12:22 +0700

    Đã comform

---
+++ src/category/category.controller.ts
@@ -0,0 +1,29 @@
+import { Controller, Get, Param } from '@nestjs/common';
+import { CategoryService } from './category.service';
+
+@Controller('category')
+export class CategoryController {
+  constructor(private categoryService: CategoryService) {}
+
+  // Lấy tất cả danh mục và sản phẩm của một danh mục cụ thể
+  @Get('/')
+  async getDefaultCategory() {
+    const category = await this.categoryService.getOneById(2);
+    const products = category?.products;
+    return {
+      message: 'get default category',
+      data: products,
+    };
+  }
+
+  // Lấy danh mục theo id
+  @Get('/:id')
+  async getCategoryById(@Param('id') id: number) {
+    const category = await this.categoryService.getOneById(id);
+    const products = category?.products;
+    return {
+      message: `get category ${id}`,
+      data: products,
+    };
+  }
+}
